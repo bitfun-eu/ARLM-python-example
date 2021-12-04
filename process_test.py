8e7e0ede176340e3baf72a3cdb03ecb48dca35db
Hua Yanghao <yanghao@users.noreply.github.com>
2021-12-04 09:35:36 +0100

    Update process_test.py

--- process_test.py
+++ process_test.py
@@ -1,20 +1,24 @@
 from os import environ as env
 from time import sleep
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 
-def target_func():
+def target_func(q):
     if 'PLAY' in env:
-        print(f"==> PLAY is: {env['PLAY']}")
+        q.put(f"==> PLAY is: {env['PLAY']}")
     else:
-        print("==> PLAY is not in environment!")
+        q.put("==> PLAY is not in environment!")
     for i in range(100):
-        print(f"child process: {i}")
+        q.put(f"child process: {i}")
         sleep(1)
 
-env['PLAY'] = 'Great'
-p = Process(target=target_func, args=(,))
-p.start()
-
-input("Please press <Enter> to finish main process.")
-p.terminate()
-p.start()
+if __name__ == "__main__":
+    env['PLAY'] = 'no-time'
+    q = Queue()
+    p = Process(target=target_func, args=(q,))
+    p.start()
+    input("Please press <Enter> to finish main process.")
+    p.terminate()
+    p.join()
+    while not q.empty():
+        print(q.get())
+    print("main process finished.")
